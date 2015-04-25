0b87af8ae7fc6def02190d176a1cb331bda234b6
Vynnyk Maksym <maksimvinnik@gmail.com>
2015-04-25 19:02:25 +0300

    external properties added to HibernateConfig

--- src/main/java/ifua/pu/mathmaps/config/HibernateConfig.java
+++ src/main/java/ifua/pu/mathmaps/config/HibernateConfig.java
@@ -3,21 +3,26 @@ package ifua.pu.mathmaps.config;
 import java.util.Properties;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan({ "ifua.pu.mathmaps.*" })
 @EnableTransactionManagement
 @Import({ SecurityConfig.class })
-//@PropertySource("classpath:database.properties")
+@PropertySource("classpath:database.properties")
 public class HibernateConfig {
+
+    @Autowired
+    Environment env;
 
     @Bean
     public SessionFactory sessionFactory() {
@@ -32,20 +37,18 @@ public class HibernateConfig {
     private Properties getHibernateProperties() {
         Properties prop = new Properties();
         prop.put("hibernate.format_sql", "true");
-        prop.put("hibernate.show_sql", "true");
-        prop.put("hibernate.dialect",
-                "ifua.pu.mathmaps.config.CustomMysqlDialect");
+        prop.put("hibernate.show_sql", env.getProperty("show_sql"));
+        prop.put("hibernate.dialect", env.getProperty("dialect"));
         return prop;
     }
 
     @Bean(name = "dataSource")
     public BasicDataSource dataSource() {
-
         BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/mathmaps");
-        ds.setUsername("root");
-        ds.setPassword("root");
+        ds.setDriverClassName(env.getProperty("driverClassName"));
+        ds.setUrl(env.getProperty("url"));
+        ds.setUsername(env.getProperty("username"));
+        ds.setPassword(env.getProperty("password"));
         return ds;
     }
 
